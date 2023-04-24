c6dcc4a8f313abd8c5dd1fc2055a81acd9182017
Francisco Del Roio <francipvb@hotmail.com>
2023-04-24 08:54:15 -0300

    Fixed publish script.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# Get the actual version to use:
+LIB_VERSION=$(dunamai from git --style pep440 --no-metadata)
+# Bump library version:
+echo "Publishing version $LIB_VERSION."
+poetry version $LIB_VERSION
+
 # https://python-poetry.org/docs/libraries/
 # https://python-poetry.org/docs/cli/#publish
-poetry publish  --build
+poetry publish --build
